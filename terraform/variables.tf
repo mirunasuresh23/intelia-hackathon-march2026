4e763a473feaacaa9edd3e2a11711f98f45804e3
ms443221 <ms443221@crownresorts.com.au>
2026-04-13 11:44:49 +1000

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,11 +13,13 @@ variable "region" {
 variable "bucket_name" {
   description = "The GCS bucket name for bronze data"
   type        = string
+  default     = "miruna-intelia-hackathon-files"
 }
 
 variable "git_repo_url" {
   description = "The URL of the Git repository for Dataform"
   type        = string
+  default     = "https://github.com/mirunasuresh/intelia-hackathon-2026.git"
 }
 
 variable "source_bucket_name" {
